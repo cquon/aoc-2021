c6f5848cd1790367a9f7669d1a2ee792e719624a
Corey Quon <corey.quon@gmail.com>
2022-01-06 00:55:39 -0800

    Day 2 Part 2

--- day2/main.go
+++ day2/main.go
@@ -38,6 +38,33 @@ forward 2 adds 2 to your horizontal position, a total of 15.
 After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
 
 Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+
+--- Part Two ---
+Based on your calculations, the planned course doesn't seem to make any sense. You find the submarine manual and discover that the process is actually slightly more complicated.
+
+In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at 0. The commands also mean something entirely different than you first thought:
+
+down X increases your aim by X units.
+up X decreases your aim by X units.
+forward X does two things:
+It increases your horizontal position by X units.
+It increases your depth by your aim multiplied by X.
+Again note that since you're on a submarine, down and up do the opposite of what you might expect: "down" means aiming in the positive direction.
+
+Now, the above example does something different:
+
+forward 5 adds 5 to your horizontal position, a total of 5. Because your aim is 0, your depth does not change.
+down 5 adds 5 to your aim, resulting in a value of 5.
+forward 8 adds 8 to your horizontal position, a total of 13. Because your aim is 5, your depth increases by 8*5=40.
+up 3 decreases your aim by 3, resulting in a value of 2.
+down 8 adds 8 to your aim, resulting in a value of 10.
+forward 2 adds 2 to your horizontal position, a total of 15. Because your aim is 10, your depth increases by 2*10=20 to a total of 60.
+After following these new instructions, you would have a horizontal position of 15 and a depth of 60. (Multiplying these produces 900.)
+
+Using this new interpretation of the commands, calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+
+
+
 */
 
 type instruction struct {
@@ -61,13 +88,10 @@ func lineParser(line []byte) interface{} {
 	}
 }
 
-func part1() {
+func part1(instructions []interface{}) {
 	position := 0
 	depth := 0
 
-	ir := reader.NewInputReader("input.txt", lineParser)
-	instructions := ir.ParseInput()
-	fmt.Println(instructions)
 	for _, i := range instructions {
 		switch i.(instruction).direction {
 		case "up":
@@ -87,6 +111,34 @@ func part1() {
 	fmt.Println(position * depth)
 }
 
+func part2(instructions []interface{}) {
+	position := 0
+	depth := 0
+	aim := 0
+
+	for _, i := range instructions {
+		switch i.(instruction).direction {
+		case "up":
+			aim -= i.(instruction).value
+			break
+		case "down":
+			aim += i.(instruction).value
+			break
+		case "forward":
+			position += i.(instruction).value
+			depth += (aim * i.(instruction).value)
+			break
+		default:
+			log.Panic("Invalid Input")
+		}
+	}
+	fmt.Println(position * depth)
+}
+
 func main() {
-	part1()
+	ir := reader.NewInputReader("input.txt", lineParser)
+	instructions := ir.ParseInput()
+
+	part1(instructions)
+	part2(instructions)
 }
